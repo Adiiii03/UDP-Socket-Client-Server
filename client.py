4ce0514e90d39bd146d1c10369de0c1576e99278
leahsaph123 <leahbogomolny@gmail.com>
2026-04-07 22:17:43 -0700

    made client stay open after receiving first msg, added error handling in case server isn't running when client is started

--- client.py
+++ client.py
@@ -10,16 +10,24 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # server address
 server_add = (SERVER_HOST, SERVER_PORT)
 
-# get user input 
-message = input("Enter a word: ")
-print(f'Your client program sends: "{message}"')
+while True:
+    # get user input 
+    message = input("Enter a word: ")
 
-# sending message to server
-client_socket.sendto(message.encode("ascii"), server_add)
+    if message == "exit":
+        break
+    
+    print(f'Your client program sends: "{message}"')
 
-# receive response from server
-response, _ = client_socket.recvfrom(1024)
-print(f'Your client program would receive the string: "{response.decode("ascii")}"')
+    # sending message to server
+    client_socket.sendto(message.encode("ascii"), server_add)
+
+    # receive response from server
+    try: 
+        response, _ = client_socket.recvfrom(1024)
+        print(f'Your client program would receive the string: "{response.decode("ascii")}"')
+    except ConnectionResetError:
+        print("Make sure to start the server before starting the client")
 
 # close socket
 client_socket.close()
